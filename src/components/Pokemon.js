a6570436b222288e75597da71d0c3535dd8f21b8
Marianna Galuppi <marianna.galuppi@gmail.com>
2022-10-05 02:07:22 +0200

    update sass folder structure, add colors based on types and relative logic

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -3,12 +3,17 @@ import { Link } from "react-router-dom";
 // CONSTANTS
 import { ROUTES } from "utils/routingConstants";
 
-function Pokemon({ name, image }) {
+function Pokemon({ name, image, mainType }) {
   return (
-    <li>
-      <Link to={ROUTES.POKEMON_DETAIL} className="pokemon">
-        <h3>{name}</h3>
-        <img src={image} alt={name} />
+    <li className="pokemon">
+      <Link to={ROUTES.POKEMON_DETAIL} className={`c-type-${mainType}`} >
+        <div className="txt-wrapper">
+          <h3>{name}</h3>
+          <small>{mainType}</small>
+        </div>
+        <div className="img-wrapper">
+          <img src={image} alt={name} />
+        </div>
       </Link>
     </li>
   );
